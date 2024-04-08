adeee0add9f82759efbf5207bf867235be4a4127
NAWAB-AS <hamid.syed.m@gmail.com>
2024-04-08 17:56:50 -0400

    fixed tiles

--- public/index.js
+++ public/index.js
@@ -24,10 +24,15 @@ var anim = {
     "Shotgun": {},
     "Rifle": {}
 }
-let tiles;
+let tilemap;
+let map = { map: [], w: 5, h: 5 };
 
-function getTile(i){
-    return tiles.get((i*128)%512, 128*Math.floor(i/9), 128, 128);
+function getTile(i) {
+    if (i != -1 && i < 16) {
+        return tilemap.get((i * 128) % 512, 128 * Math.floor((i) / 4), 128, 128);
+    } else {
+        return createImage(128, 128);
+    }
 }
 
 function BulkloadImage(prefix, index, fileType) {
@@ -46,27 +51,27 @@ function preload() {
     anim["Knife"]["move"] = BulkloadImage("images/knife/move/survivor-move_knife_", 20, ".png");
 
     anim["Knife"]["reload"] = BulkloadImage("images/knife/idle/survivor-idle_knife_", 20, ".png"); // to stop errors
-    tiles = loadImage("images/tilemap.png");
+    tilemap = loadImage("images/tilemap.png");
 }
 
 function windowResized() {
-  // 1280 x 720
-  if(windowWidth<windowHeight){
-      // width is smaller
-      ratio = windowWidth/1280;
-  } else {
-      // height is smaller
-      ratio = windowHeight/720;
-      if (ratio*1280>windowWidth){
-          ratio = windowWidth/1280;
-      }
-  }
-  resizeCanvas(1280*ratio, 720*ratio);
+    // 1280 x 720
+    if (windowWidth < windowHeight) {
+        // width is smaller
+        ratio = windowWidth / 1280;
+    } else {
+        // height is smaller
+        ratio = windowHeight / 720;
+        if (ratio * 1280 > windowWidth) {
+            ratio = windowWidth / 1280;
+        }
+    }
+    resizeCanvas(1280 * ratio, 720 * ratio);
 }
 
-function getTime(){
+function getTime() {
     let d = new Date();
-    return d.setTime( d.getTime() + d.getTimezoneOffset()*60*1000 );
+    return d.setTime(d.getTime() + d.getTimezoneOffset() * 60 * 1000);
 }
 
 function setup() {
@@ -75,16 +80,25 @@ function setup() {
     id = Math.random().toString(36).slice(2);
     frameRate(30);
     angleMode(DEGREES);
-    imageMode(CENTER)
+    imageMode(CENTER);
 
+    // connecting to server
     socket.emit("register_new_player", { id: id });
     socket.on("delay" + id, (data) => { animDelay = data.ticks; });
 
     socket.on("player_disconnected", disconnect);
     socket.on("player_update", (data) => {
         if (data.id == id) { return; }
-        players[data.id] = { x: data.x, y: data.y, id: data.id, weapon: data.weapon, stance: data.stance, tick:getTime() };
+        players[data.id] = { x: data.x, y: data.y, id: data.id, weapon: data.weapon, stance: data.stance, tick: getTime() };
     });
+
+    // setup tiles
+    for (let i = 0; i < map.h; i++) {
+        for (let j = 0; j < map.w; j++) {
+            map.map.push((i * map.w + j)%16);
+        }
+    }
+    map.map=[0, 1, 2, 2, 3, 4, 6, 7, 8, 7, 4, 11, 12, 13, 7, 4, 0, 1, 2, 7, 12, 14, 14, 14, 15];
 }
 
 function disconnect(data) {
@@ -98,24 +112,26 @@ function draw() {
     scale(ratio);
     background(200);
     animFrames++;
-    let animFrames1 = (animFrames+animDelay) % anim[me.weapon][me.stance].length;
+    let animFrames1 = (animFrames + animDelay) % anim[me.weapon][me.stance].length;
     image(anim[me.weapon][me.stance][animFrames1], me.x, me.y);
-    
+
     fill(255);
     rect(me.x, me.y, 50, 50);
     push();
     imageMode(CORNER);
-    for (let i = 0; i<4;i++){
-        for (let j = 0;j<9;j++)
-        image(getTile(i*9+j), j*130, i*130);
+    for (let i = 0; i < map.h; i++) {
+        for (let j = 0; j < map.w; j++) {
+            image(getTile(map.map[i * map.w + j]), j * 130+20, i * 130+20);
+            text(i * map.w + j, j * 130+20+64, i * 130+20+64)
+        }
     }
     pop();
     text("You", me.x, me.y);
-    socket.emit("pos", { x: me.x, y: me.y, id: id, weapon: me.weapon, stance: me.stance, tick:getTime()});
+    socket.emit("pos", { x: me.x, y: me.y, id: id, weapon: me.weapon, stance: me.stance, tick: getTime() });
     for (let i = 0; i < Object.keys(players).length; i++) {
         let player = players[Object.keys(players)[i]];
         // connection timeout
-        if (player.tick + (3*1000)<getTime()){
+        if (player.tick + (3 * 1000) < getTime()) {
             delete players[Object.keys(players)[i]];
         }
         // draw player
@@ -130,6 +146,6 @@ function mouseDragged() {
     me.x = pos(mouseX); me.y = pos(mouseY);
 }
 
-function pos(val){
+function pos(val) {
     return 1 / ratio * val;
 }
